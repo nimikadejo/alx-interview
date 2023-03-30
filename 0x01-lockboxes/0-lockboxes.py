5c3161bbe878e4658ae3a3651349561f96cca44b
nimikadejo <kadejotoluwanimi@gmail.com>
2023-03-30 21:30:17 +0100

    indentatation err

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,22 +3,21 @@
 
 
 def canUnlockAll(boxes):
-  '''
-  Args:
+    '''
+    Args:
     boxes(list): list of lists
-  Returns:
+    Returns:
     Boolean
-  '''
-  allBoxes = len(boxes)
-  openedBox = [False] * allBoxes
-  openedBox[0] = True
-  currentBox = [0]  #a list of current box opened
+    '''
+    allBoxes = len(boxes)
+    openedBox = [False] * allBoxes
+    openedBox[0] = True
+    currentBox = [0]
 
-  while currentBox:
-    index = currentBox.pop(0) #call top of stack
-    for num in boxes[index]:
-      if num >= 0 and not openedBox[num] and num < allBoxes:
-        opened[num] = True
-        currentBox.append(num)
-  return all(opened)
-
+    while currentBox:
+        index = currentBox.pop(0)
+        for num in boxes[index]:
+            if not openedBox[num] and num < allBoxes and num >= 0:
+                opened[num] = True
+                currentBox.append(num)
+    return all(opened)
