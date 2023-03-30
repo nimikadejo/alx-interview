049dd29041f3f39408d168a4ab68692c787f5951
nimikadejo <kadejotoluwanimi@gmail.com>
2023-03-30 21:44:27 +0100

    make final edits

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,19 +3,19 @@
 
 
 def canUnlockAll(boxes):
-    """
+    '''
     Determines if all boxes can be opened.
-    Returns True if all boxes can be opened, else returns False.
-    """
-    n = len(boxes)
-    unlocked = [False] * n  # Keeps track of whether each box is unlocked
-    unlocked[0] = True  # The first box is already unlocked
-    queue = [0]  # Start with the first box
-    while queue:
-        box = queue.pop(0)
-        for key in boxes[box]:
-            if key >= 0 and key < n and not unlocked[key]:
-                # The key is valid and it unlocks a new box
-                unlocked[key] = True
-                queue.append(key)
-    return all(unlocked)
+    Returns True if all boxes can be opened, else returns False
+    '''
+    allBoxes = len(boxes)
+    openedBox = [False] * allBoxes
+    openedBox[0] = True
+    currentBox = [0]  # opened box
+
+    while currentBox:
+        index = currentBox.pop(0)  # take the key at the top of the stack
+        for num in boxes[index]:
+            if num < allBoxes and num >= 0 and not openedBox[num]:
+                openedBox[num] = True
+                currentBox.append(num)  # add recently opened box to index
+    return all(openedBox)
