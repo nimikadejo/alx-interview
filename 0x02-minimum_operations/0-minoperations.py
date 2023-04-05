1db4e8cd1ec0d2222eb702a094eb5ce385013823
nimikadejo <kadejotoluwanimi@gmail.com>
2023-04-05 17:56:33 +0100

    min operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+''' Module for function '''
+
+
+def minOperations(n):
+    '''
+    Deternmines minimum number of operations to get
+    1 to n with copy all and paste
+    '''
+    content = 1
+    ops = 0
+    copy = 0
+
+    if n < 1:
+        return 0
+    if n % 10 == 0 and n > 20:
+        while content < n:
+            if n % content == 0:
+                copy += content
+                ops += 2  # copy and paste
+            else:
+                copy += content
+                ops += 1  # paste
+        content += copy
+    if (n < 20):
+        if n % 2 == 0:
+            ops = ((n/2) - 1) + 2
+        if n % 3 == 0:
+            ops = ((n/3) - 1) + 2
+    else:
+        ops = n
+    return ops
