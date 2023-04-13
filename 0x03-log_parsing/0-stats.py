3a88eefb6f5c744d3718e2101c3370be7ce8d8b6
nimikadejo <kadejotoluwanimi@gmail.com>
2023-04-13 23:13:37 +0100

    debug

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,35 +7,44 @@ A script that reads stdin line by line and computes metrics
 import sys
 
 
-def print__http_status_logs(dic, total_size):
+def print_http_status_logs(add, size):
     """ Prints std line """
-    print("File size: {:d}".format(total_size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+    print("File size: {:d}".format(size))
+    for i in sorted(add.keys()):
+        if add[i] != 0:
+            print("{}: {:d}".format(i, add[i]))
 
 
-choice = {'200': 0, '301': 0, '400': 0, '401': 0,
-          '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-n = 0
+http_status_array = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+
+count = 0
+size = 0
 
 try:
-    for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in choice.keys():
-                choice[code] += 1
-            total_size += size
-            n += 1
-
-except Exception as err:
-    pass
-
-finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(choice.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+    for ln in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            print_http_status_logs(http_status_array, size)
+        status_list = ln.split()
+        count += 1
+        try:
+            size += int(status_list[-1])
+        except ValueError:
+            pass
+        try:
+            if status_list[-2] in http_status_array:
+                http_status_array[status_list[-2]] += 1
+        except IndexError:
+            pass
+    print_http_status_logs(http_status_array, size)
+except KeyboardInterrupt:
+    print_http_status_logs(http_status_array, size)
+    raise KeyboardInterrupt
