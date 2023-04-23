a0ed1251ba134d3698fb08fc449c85ed29eb8719
nimikadejo <kadejotoluwanimi@gmail.com>
2023-04-23 19:02:37 +0100

    test

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,20 +9,20 @@ def validUTF8(data):
     bytes_left = 0
 
     #  Loop through every byte in input data set
-    for byte in data:
+    for b in data:
         #  If no bytes left to read,
         #  determine the number of bytes for next character
         if bytes_left == 0:
-            if byte >> 5 == 0b110:
+            if b >> 5 == 0b110 or b >> 5 == 0b1110:
                 bytes_left = 1
-            elif byte >> 4 == 0b1110:
+            elif b >> 4 == 0b1110:
                 bytes_left = 2
-            elif byte >> 3 == 0b11110:
+            elif b >> 3 == 0b11110:
                 bytes_left = 3
-            elif byte >> 7 == 1:
+            elif b >> 7 == 0b1:
                 return False
         else:
-            if byte >> 6 != 0b10:
+            if b >> 6 != 0b10:
                 return False
             bytes_left -= 1
 
