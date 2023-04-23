3452e456f4ab64ce75cf8440c7305f0334bf7c7b
nimikadejo <kadejotoluwanimi@gmail.com>
2023-04-23 18:45:52 +0100

    change var name

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,25 +4,29 @@ A script that determines if a given data set represents a valid UTF-8 encoding.
 '''
 
 
-def validUTF8(data):
-    bytes = 0
+def valid_utf8(utf8_data):
+    #  Initialize the number of bytes left to read for a character
+    bytes_left = 0
 
     #  Loop through every byte in input data set
-    for byte in data:
-        if bytes == 0:
+    for byte in utf8_data:
+        #  If no bytes left to read,
+        #  determine the number of bytes for next character
+        if bytes_left == 0:
             if byte >> 5 == 0b110:
-                bytes = 1
+                bytes_left = 1
             elif byte >> 4 == 0b1110:
-                bytes = 2
+                bytes_left = 2
             elif byte >> 3 == 0b11110:
-                bytes = 3
+                bytes_left = 3
             elif byte >> 7 == 1:
                 return False
         else:
             if byte >> 6 != 0b10:
                 return False
-            bytes -= 1
-    if bytes > 0:
+            bytes_left -= 1
+
+    if bytes_left > 0:
         return False
 
     return True
