1e787a19a261c5c6c18da7977c8b7f00db1dff6d
nimikadejo <kadejotoluwanimi@gmail.com>
2023-04-27 20:47:43 +0100

    checking

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -6,71 +6,89 @@ A script that solves the N queens problem.
 
 import sys
 
-# Check if N is a valid integer
-try:
-    N = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
 
-# Check if N is greater than or equal to 4
-if N < 4:
-    print("N must be at least 4")
-    sys.exit(1)
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
 
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        exit(1)
 
-# Function to check if a position is safe for a queen
-def isSafe(board, row, col):
-    # Check this row on left side
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
 
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
+    solutions = []
+    placed_queens = []  # coordinates format [row, column]
+    stop = False
+    rows = 0
+    c = 0
 
-    # Check lower diagonal on left side
-    for i, j in zip(range(row, N), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
+    # iterate thru rows
+    while rows < n:
+        goback = False
+        # iterate thru columns
+        while c < n:
+            # check is current column is safe
+            safe = True
+            for cord in placed_queens:
+                col = cord[1]
+                if (col == c or col + (rows-cord[0]) == c
+                        or col - (rows-cord[0]) == c):
+                    safe = False
+                    break
 
-    return True
+            if not safe:
+                if c == n - 1:
+                    goback = True
+                    break
+                c += 1
+                continue
 
+            # place queen
+            cords = [rows, c]
+            placed_queens.append(cords)
+            # if last row, append solution and reset all to last unfinished row
+            # and last safe column in that row
+            if rows == n - 1:
+                solutions.append(placed_queens[:])
+                for cord in placed_queens:
+                    if cord[1] < n - 1:
+                        rows = cord[0]
+                        c = cord[1]
+                for i in range(n - rows):
+                    placed_queens.pop()
+                if rows == n - 1 and c == n - 1:
+                    placed_queens = []
+                    stop = True
+                rows -= 1
+                c += 1
+            else:
+                c = 0
+            break
+        if stop:
+            break
+        # on fail: go back to previous row
+        # and continue from last safe column + 1
+        if goback:
+            rows -= 1
+            while rows >= 0:
+                c = placed_queens[rows][1] + 1
+                del placed_queens[rows]  # delete previous queen coordinates
+                if c < n:
+                    break
+                rows -= 1
+            if rows < 0:
+                break
+            continue
+        rows += 1
 
-# Recursive function to solve N Queens problem
-def solveNQueens(board, col):
-    # Base case: If all queens are placed, print the solution
-    if col == N:
-        for i in range(N):
-            for j in range(N):
-                print(board[i][j], end=' ')
-            print()
-        print()
-        return True
-
-    # Initialize the flag to keep track of safe position for queen
-    res = False
-
-    # Check every row in this column
-    for i in range(N):
-        if isSafe(board, i, col):
-            # Place the queen
-            board[i][col] = 1
-
-            # Check if it leads to a solution
-            res = solveNQueens(board, col+1) or res
-
-            # Backtrack and remove the queen from this position
-            board[i][col] = 0
-
-    return res
-
-
-# Initialize the board with 0s
-board = [[0 for x in range(N)] for y in range(N)]
-
-# Call the recursive function to solve the problem
-if not solveNQueens(board, 0):
-    print("No solution exists")
+    for idx, val in enumerate(solutions):
+        if idx == len(solutions) - 1:
+            print(val, end='')
+        else:
+            print(val)
