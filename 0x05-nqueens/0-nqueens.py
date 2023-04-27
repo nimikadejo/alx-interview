dcdb34fbad641ec3f2ffa063d5b7bc8ea998be41
nimikadejo <kadejotoluwanimi@gmail.com>
2023-04-27 20:53:37 +0100

    pushing

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -25,11 +25,11 @@ if __name__ == '__main__':
     solutions = []
     placed_queens = []  # coordinates format [row, column]
     stop = False
-    rows = 0
+    r = 0
     c = 0
 
     # iterate thru rows
-    while rows < n:
+    while r < n:
         goback = False
         # iterate thru columns
         while c < n:
@@ -37,8 +37,8 @@ if __name__ == '__main__':
             safe = True
             for cord in placed_queens:
                 col = cord[1]
-                if (col == c or col + (rows-cord[0]) == c
-                        or col - (rows-cord[0]) == c):
+                if (col == c or col + (r-cord[0]) == c
+                        or col - (r-cord[0]) == c):
                     safe = False
                     break
 
@@ -50,22 +50,22 @@ if __name__ == '__main__':
                 continue
 
             # place queen
-            cords = [rows, c]
+            cords = [r, c]
             placed_queens.append(cords)
             # if last row, append solution and reset all to last unfinished row
             # and last safe column in that row
-            if rows == n - 1:
+            if r == n - 1:
                 solutions.append(placed_queens[:])
                 for cord in placed_queens:
                     if cord[1] < n - 1:
-                        rows = cord[0]
+                        r = cord[0]
                         c = cord[1]
-                for i in range(n - rows):
+                for i in range(n - r):
                     placed_queens.pop()
-                if rows == n - 1 and c == n - 1:
+                if r == n - 1 and c == n - 1:
                     placed_queens = []
                     stop = True
-                rows -= 1
+                r -= 1
                 c += 1
             else:
                 c = 0
@@ -75,17 +75,17 @@ if __name__ == '__main__':
         # on fail: go back to previous row
         # and continue from last safe column + 1
         if goback:
-            rows -= 1
-            while rows >= 0:
-                c = placed_queens[rows][1] + 1
-                del placed_queens[rows]  # delete previous queen coordinates
+            r -= 1
+            while r >= 0:
+                c = placed_queens[r][1] + 1
+                del placed_queens[r]  # delete previous queen coordinates
                 if c < n:
                     break
-                rows -= 1
-            if rows < 0:
+                r -= 1
+            if r < 0:
                 break
             continue
-        rows += 1
+        r += 1
 
     for idx, val in enumerate(solutions):
         if idx == len(solutions) - 1:
