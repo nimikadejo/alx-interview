9dbb3401b281f3e4bc8837faa22ff6295c30fe24
nimikadejo <kadejotoluwanimi@gmail.com>
2023-05-25 23:29:32 +0100

    pycode style error fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 """
-module to find perimeter of the island
+Module to find perimeter of the island.
 """
 
 bound_4 = set()
@@ -10,37 +11,39 @@ bound_1 = set()
 
 
 def boundary(grid, i, j):
-    """Find cells with either 4, 3, 2 or 1 exposed boundary and add them to
-       appropriate set
-       Args:
-           grid (list): 2d list
-           i (int): row number
-           j (int): column number
+    """
+    Find cells with either 4, 3, 2, or 1 exposed boundary and add them to
+    the appropriate set.
+
+    Args:
+        grid (list): 2D list
+        i (int): row number
+        j (int): column number
     """
     boundaries = 0
     try:
         if i == 0:
             boundaries += 1
-        elif grid[i-1][j] == 0:
+        elif grid[i - 1][j] == 0:
             boundaries += 1
-    except:
+    except IndexError:
         boundaries += 1
     try:
-        if grid[i+1][j] == 0:
+        if grid[i + 1][j] == 0:
             boundaries += 1
-    except:
+    except IndexError:
         boundaries += 1
     try:
-        if grid[i][j+1] == 0:
+        if grid[i][j + 1] == 0:
             boundaries += 1
-    except:
+    except IndexError:
         boundaries += 1
     try:
         if j == 0:
             boundaries += 1
-        elif grid[i][j-1] == 0:
+        elif grid[i][j - 1] == 0:
             boundaries += 1
-    except:
+    except IndexError:
         boundaries += 1
 
     if boundaries == 1:
@@ -54,7 +57,8 @@ def boundary(grid, i, j):
 
 
 def island_perimeter(grid):
-    """function returns the perimeter of the island described in grid
+    """
+    Function that returns the perimeter of the island described in the grid.
     """
     if grid == []:
         return 0
@@ -66,5 +70,5 @@ def island_perimeter(grid):
                 boundary(grid, i, j)
                 if len(bound_4) != 0:
                     return 4
-    perimeter = (len(bound_3) * 3) + (len(bound_2) * 2) + (len(bound_1))
+    perimeter = (len(bound_3) * 3) + (len(bound_2) * 2) + len(bound_1)
     return perimeter
